37247e854a87cee440343979bf586ca5138ac86b
saeed k <sd198867@gmail.com>
2021-04-21 09:46:01 +0430

    First Commit - project completed but not dynamic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import Blog from "./components/Blog";
+import ContactUs from "./components/ContactUs";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="page-wrapper">
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/blog" component={Blog} />
+          <Route path="/contact-us" component={ContactUs} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <nav className="top-nav">
+      <Link to="/">
+        <h3 className="logo">Logo</h3>
+      </Link>
+      <ul className="nav-links">
+        <Link to="/blog">
+          <li>Blog</li>
+        </Link>
+        <Link to="/contact-us">
+          <li>Contact Us</li>
+        </Link>
+        <Link to="/about">
+          <li>About</li>
+        </Link>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
